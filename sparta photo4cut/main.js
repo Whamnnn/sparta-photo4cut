851103a6caf4a99b929f02f31dc129dc7d457528
Whamnnn <71070727+Whamnnn@users.noreply.github.com>
2023-02-03 23:41:34 +0900

    Add files via upload

---
+++ sparta photo4cut/main.js
@@ -0,0 +1,32 @@
+<div
+	id="image1"
+	class="photo-frame"
+	onmouseover="hideText(1)"
+	onmouseout="showText(1)"
+	onclick="alertText(1)"
+>
+	<span id="desc1" class="photo-description">
+		첫 번째 이미지 설명
+	</span>
+</div>
+<div
+	id="image2" class="photo-frame"
+	onmouseover="hideText(2)"
+	onmouseout="showText(2)"
+	onclick="alertText(2)"
+>
+	<span id="desc2" class="photo-description">
+		두 번째 이미지 설명
+	</span>
+</div>
+<div
+	id="image3"
+	class="photo-frame"
+	onmouseover="hideText(3)"
+	onmouseout="showText(3)"
+	onclick="alertText(3)"
+>
+	<span id="desc3" class="photo-description">
+	세 번째 이미지 설명
+</span>
+</div>
